41ba09380447366f3cdc0efda8eb784fa725d0cc
Anita Toth <haluska99@hotmail.com>
2021-02-11 16:59:05 +0000

    completed activity with all 4 tests passing

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,33 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false if the matrix is empty", function () {
+    const result = wordSearch([
+      [],
+      [],
+      [],
+
+    ], 'FRANK')
+
+    assert.isFalse(result);
+
+  });
+
+  it("should return true if the word is present", function () {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programming with Anita Toth, Victor Fung
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  //console.log("=============================start of function");
+  if (letters[0].length === 0) {
+    //console.log("return false from 0 length")
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {     //loop through rows
+    if (l.includes(word)) {          //if row includes word return true
+      //console.log("returning true");
+      return true;
+    }
+  }
+  let verticalJoin = [];
+  let verticalWord;
+
+  for (let i = 0; i < letters[i].length; i++) {
+    verticalJoin = [];
+    for (let j = 0; j < letters.length; j++) {
+      verticalJoin.push(letters[j][i]);
+      verticalWord = verticalJoin.join("");
+      if (verticalWord.includes(word)) {
+        //console.log("returning true");
+        return true;
+      }
+    }
+    // console.log("vertialjoin: ", verticalJoin);
+    // console.log("verticalWord: " + verticalWord);
+  }
+  //console.log("returning false");
+  return false;
+};
+
+
+module.exports = wordSearch;
+
