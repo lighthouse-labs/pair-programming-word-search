b7928233b7d1f454062371c52b9c0b3aec0d2aa3
Michaela-K <michaelakodes@gmail.com>
2022-01-19 20:28:47 +0000

    word search update

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
+        // console.log(l)
         if (l.includes(word)) return true
     }
+    const verticalArraysnumber = letters[0].length
+    // console.log(verticalArraysnumber)
+    let vertical = []
+    for (let r = 0; r < letters[0].length; r++) {
+        let verticalWordArray = []
+        for (let c = 0; c <= verticalArraysnumber; c++) {
+            verticalWordArray.push(letters[c][r])
+        }
+        let verticalWord = verticalWordArray.join('')
+        vertical.push(verticalWord)
+    }
+    // console.log(vertical)
+    for (item of vertical) {
+        console.log(item)
+        console.log(item.includes(word)) 
+        if (item.includes(word)) {
+            return true;
+        }
+    }
+    return false
 }
 
 module.exports = wordSearch
