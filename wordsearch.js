4db81bae172137d74fda3549f32bee898bb33bc8
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-25 14:51:28 -0400

    Word searcher completed
    Co-authored-by: Aden McCarville <HandsomePeasant@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    if (letters.length === 0) {
+        return false;
     }
+    
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    for (let row = 0; row < letters.length; row++) {
+        let string = "";
+        for (let col = 0; col < horizontalJoin[0].length; col++) {
+            string += letters[col][row];
+            if (string.includes(word)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
